bb90036eab0949328a901c337e55f2f613ced029
unknown <desarrollowebaps@gmail.com>
2021-09-11 22:12:10 -0400

    CARRITO DE VENTAS UPDATE SABADO 11/09/2021 22:14

---
+++ js/carrito.js
@@ -0,0 +1,61 @@
+
+let btnCarrito = document.querySelector("#imgCarrito");
+let contBasePedido = document.querySelector("#contBasePedido");
+let iconResponsive = document.querySelector("#iconResponsive");
+let containerHeaderTopC2 = document.querySelector("#containerHeaderTopC2");
+
+
+btnCarrito.addEventListener("click" , () =>{
+    btnCarrito.classList.toggle("imgCarritoJS");
+    contBasePedido.classList.toggle("contBasePedidoJS");
+});
+
+iconResponsive.addEventListener("click" , () =>{
+    containerHeaderTopC2.classList.toggle("containerHeaderTopC2JS");
+})
+
+
+
+
+
+
+
+//IMAGENES INSERTADAS DINAMICAMENTE/////////////////////////
+const img1 = document.querySelector("#img1");
+img1.classList.add("img1JS");
+const img2 = document.querySelector("#img2");
+img2.classList.add("img2JS");
+const img3 = document.querySelector("#img3");
+img3.classList.add("img3JS");
+const img4 = document.querySelector("#img4");
+img4.classList.add("img4JS");
+/*
+const img4 = document.querySelector("#img4");
+img4.classList.add("img4JS");
+const img4 = document.querySelector("#img4");
+img4.classList.add("img4JS");
+const img4 = document.querySelector("#img4");
+img4.classList.add("img4JS");
+const img4 = document.querySelector("#img4");
+img4.classList.add("img4JS");
+const img4 = document.querySelector("#img4");
+img4.classList.add("img4JS");
+const img4 = document.querySelector("#img4");
+img4.classList.add("img4JS");
+const img4 = document.querySelector("#img4");
+img4.classList.add("img4JS");
+const img4 = document.querySelector("#img4");
+img4.classList.add("img4JS");
+*/
+
+
+
+
+
+
+
+
+
+
+
+
